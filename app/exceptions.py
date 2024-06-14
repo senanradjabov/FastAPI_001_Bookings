d7ce9863ee8777f0bbbd39a0693d9a76619f0f53
senanradjabov <iamtheemperor06@gmail.com>
2024-06-14 21:50:34 +0400

    add exceptions

---
+++ app/exceptions.py
@@ -0,0 +1,19 @@
+from fastapi import HTTPException, status
+
+
+class BookingException(HTTPException):
+    status_code = 500
+    detail = ""
+
+    def __init__(self):
+        super().__init__(status_code=self.status_code, detail=self.detail)
+
+
+class UserAlreadyExistsException(BookingException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = "Пользователь уже существует"
+
+
+class RoomCannotBeBooked(BookingException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = "Не осталось свободных номеров"
