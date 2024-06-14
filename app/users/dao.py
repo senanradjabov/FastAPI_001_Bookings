8b90cf0836cf4edf034d3ff3259e6695ef709fbb
senanradjabov <iamtheemperor06@gmail.com>
2024-06-14 21:49:57 +0400

    add method for adding user

--- app/users/dao.py
+++ app/users/dao.py
@@ -1,6 +1,19 @@
+from sqlalchemy import insert
+
 from app.dao.base import BaseDAO
+from app.database import async_session_maker
 from app.users.models import Users
 
 
 class UsersDAO(BaseDAO):
     model = Users
+
+    @classmethod
+    async def add(cls, **data):
+        async with async_session_maker() as session:
+            query = insert(cls.model).values(**data).returning(cls.model.id)
+
+            result = await session.execute(query)
+            await session.commit()
+
+        return result.scalar()
